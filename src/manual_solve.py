c75b536197b776e71d17c2613b254a7e7e1bfa0b
Mayank Dwivedi <mayank.7514@gmail.com>
2021-11-29 17:36:31 +0000

    Commit 2: 2 tasks complete

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -19,8 +19,55 @@ def solve_aabf363d(x):
     x[x > 0] = colorEnd
     return x
 
-def solve_b2862040(x):
-    return x
+def solve_c8cbb738(x):
+    data = np.array(x)
+    
+    uniq, counts = np.unique(data, return_counts=True)
+
+    index = 0
+    for i in counts:
+        if i == max(counts):
+            break
+        index += 1
+    backG = uniq[index]
+    uniq = np.delete(uniq, index)
+    counts = np.delete(counts, index)
+
+    listOfCoordinates={}
+    dim_out_arr = []
+    c = lambda x,y: [list(c) for c in zip(x, y)]
+
+    for i in range(len(uniq)):
+        a = np.where(data == uniq[i])
+        listOfCoordinates[uniq[i]] = c(a[0],a[1])
+        dim_out_arr.append(max(a[0]) - min(a[0]))
+        dim_out_arr.append(max(a[1]) - min(a[1]))
+
+    dim_out = max(dim_out_arr)
+
+    def _sliceCoordinates(val, dim):
+        d=np.array(val)
+        #print(d)
+        minx=min(d.T[0])
+        miny=min(d.T[1])
+        d=d-[minx,miny]
+
+        if dim in d.T[0]:
+            if dim in d.T[1]:
+                pass
+            else:
+                d.T[1]=d.T[1]+(dim-max(d.T[1]))/2
+        else:
+            d.T[0]=d.T[0]+(dim-max(d.T[0]))/2
+        return d
+
+    newCoordinates={}
+    output_matrix = np.full((dim_out+1, dim_out+1), backG)
+    for key in listOfCoordinates.keys():
+        for n_coor in _sliceCoordinates(listOfCoordinates[key], dim_out):
+            output_matrix[tuple(n_coor)] = key
+
+    return output_matrix
 
 def solve_05269061(x):
     return x
