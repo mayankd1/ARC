f710eb4b77415c1c0a853ce13c64a1cee61ef487
Mayank Dwivedi <mayank.7514@gmail.com>
2021-11-29 19:04:37 +0000

    Commit 3: With comments

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -10,6 +10,7 @@ import re
 ### result. Name them according to the task ID as in the three
 ### examples below. Delete the three examples. The tasks you choose
 ### must be in the data/training directory, not data/evaluation.
+
 def solve_aabf363d(x):
     # Get the shape of i/p NP Array
     row,col=x.shape
@@ -22,50 +23,64 @@ def solve_aabf363d(x):
     # Return O/p
     return x
 
+# Solution for Training example c8cbb738.json
 def solve_c8cbb738(x):
     data = np.array(x)
-    
+    # Storing Colors and their occurences
     uniq, counts = np.unique(data, return_counts=True)
-
+    # Check the indexes for background color
     index = 0
     for i in counts:
         if i == max(counts):
             break
         index += 1
+    # Storing background color
     backG = uniq[index]
+    # Deleting the Color code and counts to create new lists with primary colors
     uniq = np.delete(uniq, index)
     counts = np.delete(counts, index)
 
+    # Declaring variables to store (key,value) of Color(as keys) and Coordinates(as values) 
     listOfCoordinates={}
+    # Dimension of O/p array
     dim_out_arr = []
+    # Lambda Fn to extract x,y coordinates
     c = lambda x,y: [list(c) for c in zip(x, y)]
-
+    # Itering over primary colors to get the coordinates for each
     for i in range(len(uniq)):
         a = np.where(data == uniq[i])
         listOfCoordinates[uniq[i]] = c(a[0],a[1])
         dim_out_arr.append(max(a[0]) - min(a[0]))
         dim_out_arr.append(max(a[1]) - min(a[1]))
-
+    # Using max Fn to get the dimensions of O/p array
     dim_out = max(dim_out_arr)
 
+    # Method to slice and traverse the Color matrix and determining it's position on O/p matrix
     def _sliceCoordinates(val, dim):
         d=np.array(val)
-        #print(d)
+        # Checking minimum coordinates for x and y axis
         minx=min(d.T[0])
         miny=min(d.T[1])
         d=d-[minx,miny]
-
+        # Checking the Color axis if it contains the dimension of O/p array
+        # To determine if the shape of Color grid will fit to O/p  array or requires a shift in X or Y axis
         if dim in d.T[0]:
             if dim in d.T[1]:
                 pass
+        # If Color grid does not fit in Y-axis, make a shift to match the O/p
             else:
                 d.T[1]=d.T[1]+(dim-max(d.T[1]))/2
+        # If Color grid does not fit in X-axis, make a shift to match the O/p
         else:
             d.T[0]=d.T[0]+(dim-max(d.T[0]))/2
+        # Return the index for each color wrt output
         return d
 
+    # Declaring a dict to store Color(as Keys) and final indexes(as Values)
     newCoordinates={}
+    # Creating an output matrix, filled with the background color
     output_matrix = np.full((dim_out+1, dim_out+1), backG)
+    # Iterating over each key to place the colors at the desired x,y coordinates
     for key in listOfCoordinates.keys():
         for n_coor in _sliceCoordinates(listOfCoordinates[key], dim_out):
             output_matrix[tuple(n_coor)] = key
